5f33feac65543b321bde729b83ac21606786e73c
Batzi1337 <28926863+Batzi1337@users.noreply.github.com>
2023-12-22 15:13:08 +0100

    Create go.mod and update inbox.go

--- inbox.go
+++ inbox.go
@@ -23,15 +23,15 @@ const (
 	TrashFolder   Folder       = "Trash"
 )
 
-type Bot struct {
+type Inbox struct {
 	cred   *Credentials
 	client *client.Client
 }
 
 // New creates a new Bot and authenticate with the given credentials.
-func New(provider ImapProvider, cred *Credentials) (*Bot, error) {
-	bot := new(Bot)
-	bot.cred = cred
+func New(provider ImapProvider, cred *Credentials) (*Inbox, error) {
+	inbox := new(Inbox)
+	inbox.cred = cred
 
 	// Connect to server
 	client, err := client.DialTLS(string(provider), nil)
@@ -44,15 +44,15 @@ func New(provider ImapProvider, cred *Credentials) (*Bot, error) {
 		return nil, err
 	}
 
-	bot.client = client
+	inbox.client = client
 
-	return bot, nil
+	return inbox, nil
 }
 
 // DeleteAllMessagesInFolder deletes all messages in the given folder.
 // When expunge is set to "false", no "\DELETED" flag is set (safe mode). When set to "true", all messages removed permenantly.
-func (b *Bot) DeleteAllMessagesInFolder(expunge bool, folder Folder) error {
-	mbox, err := selectFolder(b, folder)
+func (i *Inbox) DeleteAllMessagesInFolder(expunge bool, folder Folder) error {
+	mbox, err := selectFolder(i, folder)
 	if err != nil {
 		return err
 	}
@@ -64,13 +64,13 @@ func (b *Bot) DeleteAllMessagesInFolder(expunge bool, folder Folder) error {
 		return nil
 	}
 
-	return deleteMessagesPermanently(b, delSeqSet)
+	return deleteMessagesPermanently(i, delSeqSet)
 }
 
 // DeleteMessagesInFolderFromAddress sets the "\DELETED" flag to all messages sent from the given addresses.
 // When expunge is set to "false", no "\DELETED" flag is set (safe mode). When set to "true", messages matching to the given
 // addresses are removed permenantly.
-func (b *Bot) DeleteMessagesInFolderFromAddress(expunge bool, folder Folder, address ...string) error {
+func (b *Inbox) DeleteMessagesInFolderFromAddress(expunge bool, folder Folder, addr ...string) error {
 	mbox, err := selectFolder(b, folder)
 	if err != nil {
 		return err
@@ -84,7 +84,7 @@ func (b *Bot) DeleteMessagesInFolderFromAddress(expunge bool, folder Folder, add
 
 	delSeqSet := new(imap.SeqSet)
 
-	go compare(address, messages, delSeqSet)
+	compare(addr, messages, delSeqSet)
 
 	if err := <-errChan; err != nil {
 		return err
@@ -127,7 +127,7 @@ func printMessagesToDelete(msgMapChan chan map[string]string) {
 }
 
 // deleteMessagesPermanently sets the deleted flag and expunge them.
-func deleteMessagesPermanently(b *Bot, delSeqSet *imap.SeqSet) error {
+func deleteMessagesPermanently(b *Inbox, delSeqSet *imap.SeqSet) error {
 	if err := b.client.Store(delSeqSet, imap.StoreItem(imap.AddFlags), []interface{}{imap.DeletedFlag}, nil); err != nil {
 		return err
 	}
@@ -136,7 +136,7 @@ func deleteMessagesPermanently(b *Bot, delSeqSet *imap.SeqSet) error {
 }
 
 // selectFolder sets the given folder as selected mailbox.
-func selectFolder(b *Bot, folder Folder) (*imap.MailboxStatus, error) {
+func selectFolder(b *Inbox, folder Folder) (*imap.MailboxStatus, error) {
 	mbox, err := b.client.Select(string(folder), false)
 	if err != nil {
 		return nil, err
@@ -147,12 +147,12 @@ func selectFolder(b *Bot, folder Folder) (*imap.MailboxStatus, error) {
 	return mbox, nil
 }
 
-func (b *Bot) Logout() error {
+func (b *Inbox) Logout() error {
 	return b.client.Logout()
 }
 
 // fetchAllMessages fetches all messages in the selected mailbox.
-func fetchAllMessages(mbox *imap.MailboxStatus, b *Bot, messages chan *imap.Message) error {
+func fetchAllMessages(mbox *imap.MailboxStatus, b *Inbox, messages chan *imap.Message) error {
 	seqSet := new(imap.SeqSet)
 	seqSet.AddRange(1, mbox.Messages)
 	if err := b.client.Fetch(seqSet, []imap.FetchItem{imap.FetchEnvelope}, messages); err != nil {
@@ -177,4 +177,53 @@ func compareMessageWithAddresses(msg *imap.Message, address []string, mapChan ch
 	}
 
 	mapChan <- m
+}
+
+func main() {
+	// Credentials
+	cred := &Credentials{
+		Username: "username",
+		Password: "password",
+	}
+
+	// Create new inbox
+	inbox, err := New(GMX, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete all messages in the inbox folder
+	if err := inbox.DeleteAllMessagesInFolder(true, InboxFolder); err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete all messages in the spam folder
+	if err := inbox.DeleteAllMessagesInFolder(true, GmxSpamFolder); err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete all messages in the trash folder
+	if err := inbox.DeleteAllMessagesInFolder(true, TrashFolder); err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete all messages from the given addresses in the inbox folder
+	if err := inbox.DeleteMessagesInFolderFromAddress(true, InboxFolder, "address1", "address2"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete all messages from the given addresses in the spam folder
+	if err := inbox.DeleteMessagesInFolderFromAddress(true, GmxSpamFolder, "address1", "address2"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete all messages from the given addresses in the trash folder
+	if err := inbox.DeleteMessagesInFolderFromAddress(true, TrashFolder, "address1", "address2"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Logout
+	if err := inbox.Logout(); err != nil {
+		log.Fatal(err)
+	}
 }
